976e8e2dc1b44b4df728258bb27d1a42428f1abf
jaya9114 <131508046+jaya9114@users.noreply.github.com>
2024-02-04 19:19:58 +0530

    add changes

--- lib/main.dart
+++ lib/main.dart
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
           theme: theme,
           title: 'dr_app',
           debugShowCheckedModeBanner: false,
-          initialRoute: AppRoutes.progressWeeklyPage,
+          initialRoute: AppRoutes.progressHourlyPage,
           routes: AppRoutes.routes,
         );
       },
--- lib/presentation/progress_hourly_page/progress_hourly_page.dart
+++ lib/presentation/progress_hourly_page/progress_hourly_page.dart
@@ -18,71 +18,99 @@ class ProgressHourlyPageState extends State<ProgressHourlyPage>
     with AutomaticKeepAliveClientMixin<ProgressHourlyPage> {
   @override
   bool get wantKeepAlive => true;
-  @override
-  Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: SizedBox(
-          width: SizeUtils.width,
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                SizedBox(height: 20.v),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 20.h),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        padding: EdgeInsets.symmetric(
-                          horizontal: 7.h,
-                          vertical: 8.v,
-                        ),
-                        decoration: AppDecoration.outlineGray300.copyWith(
-                          borderRadius: BorderRadiusStyle.roundedBorder4,
-                        ),
-                        child: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            CustomImageView(
-                              imagePath: ImageConstant.imgArrowLeftBlueGray600,
-                              height: 14.adaptSize,
-                              width: 14.adaptSize,
-                              margin: EdgeInsets.symmetric(vertical: 3.v),
-                            ),
-                            Padding(
-                              padding: EdgeInsets.only(left: 18.h),
-                              child: Text(
-                                "09:00-15:00",
-                                style: CustomTextStyles
-                                    .titleSmallOnPrimaryContainerMedium,
-                              ),
-                            ),
-                            CustomImageView(
-                              imagePath: ImageConstant.imgArrowRightBlueGray600,
-                              height: 14.adaptSize,
-                              width: 14.adaptSize,
-                              margin: EdgeInsets.only(
-                                left: 18.h,
-                                top: 3.v,
-                                bottom: 3.v,
-                              ),
-                            ),
-                          ],
-                        ),
+ 
+ @override
+Widget build(BuildContext context) {
+  return SafeArea(
+    child: Scaffold(
+      appBar: AppBar(
+        title: Text("Progress"),
+      ),
+      body: SizedBox(
+        width: SizeUtils.width,
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              SizedBox(height: 20.v),
+              Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.h),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    // New Titles
+                    Text(
+                      "Daily",
+                      // style: CustomTextStyles.titleMediumOnPrimary,
+                    ),
+                    SizedBox(height: 10.v),
+                    Text(
+                      "Weekly",
+                      // style: CustomTextStyles.titleMediumOnPrimary,
+                    ),
+                    SizedBox(height: 10.v),
+                    Text(
+                      "Monthly",
+                      // style: CustomTextStyles.titleMediumOnPrimary,
+                    ),
+                    SizedBox(height: 10.v),
+                    Text(
+                      "Yearly",
+                      // style: CustomTextStyles.titleMediumOnPrimary,
+                    ),
+                    SizedBox(height: 20.v),
+                    
+                    // Rest of the existing code
+                    Container(
+                      padding: EdgeInsets.symmetric(
+                        horizontal: 7.h,
+                        vertical: 8.v,
                       ),
-                      SizedBox(height: 20.v),
-                      _buildCalorieTrackerList(context),
-                    ],
-                  ),
+                      decoration: AppDecoration.outlineGray300.copyWith(
+                        borderRadius: BorderRadiusStyle.roundedBorder4,
+                      ),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          CustomImageView(
+                            imagePath: ImageConstant.imgArrowLeftBlueGray600,
+                            height: 14.adaptSize,
+                            width: 14.adaptSize,
+                            margin: EdgeInsets.symmetric(vertical: 3.v),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.only(left: 18.h),
+                            child: Text(
+                              "09:00-15:00",
+                              style: CustomTextStyles
+                                  .titleSmallOnPrimaryContainerMedium,
+                            ),
+                          ),
+                          CustomImageView(
+                            imagePath: ImageConstant.imgArrowRightBlueGray600,
+                            height: 14.adaptSize,
+                            width: 14.adaptSize,
+                            margin: EdgeInsets.only(
+                              left: 18.h,
+                              top: 3.v,
+                              bottom: 3.v,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(height: 20.v),
+                    _buildCalorieTrackerList(context),
+                  ],
                 ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
-    );
-  }
+    ),
+  );
+}
+  
 
   /// Section Widget
   Widget _buildCalorieTrackerList(BuildContext context) {
